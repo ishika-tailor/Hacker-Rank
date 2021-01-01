b519be57086e88e95f869113fdc027ea3b64a6b2
Ishika Tailor <66948433+ishika-tailor@users.noreply.github.com>
2021-01-01 22:46:11 +0530

    Add files via upload

---
+++ Day-13 Abstract class.cpp
@@ -0,0 +1,39 @@
+//Day:13 Abstract class
+
+class Book{
+    protected:
+        string title;
+        string author;
+    public:
+        Book(string t,string a){
+            title=t;
+            author=a;
+        }
+        virtual void display()=0;
+
+};
+
+class MyBook : Book {
+    
+    private:
+    int price;
+    public:
+    MyBook(string t,string a,int price):Book(t,a){
+            this->price = price;
+    }
+    void display(){
+        cout << "Title: " << title << endl << "Author: " << author          << endl << "Price: " << price << endl;
+    }
+    
+};
+
+int main() {
+    string title,author;
+    int price;
+    getline(cin,title);
+    getline(cin,author);
+    cin>>price;
+    MyBook novel(title,author,price);
+    novel.display();
+    return 0;
+}
